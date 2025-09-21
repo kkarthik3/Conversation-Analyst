45eeb14ef1906761523f38c57d73e1ad5d45862a
Karthikeyan K <124917539+kkarthik3@users.noreply.github.com>
2025-09-21 20:05:50 +0530

    doc: Added docstring

--- api/llm.py
+++ api/llm.py
@@ -1,3 +1,11 @@
+
+"""LLM CHAINING AND STREAMLIT APPLICATION.
+
+This script sets up a basic LangChain application using two different Groq-powered
+language models and will be used internally for summarization, topic modelling and etc..
+"""
+
+
 from langchain_core.prompts import PromptTemplate
 from langchain.schema import StrOutputParser
 from langchain_core.runnables import RunnableLambda
@@ -9,26 +17,35 @@ from langchain_groq import ChatGroq
 
 from dotenv import load_dotenv
 
+# Load environment variables from a .env file.
 load_dotenv()
 
+# --- Language Model Configuration ---
+
+# ChatGroq instance configured for JSON output.
 llm_groq = ChatGroq(
     model="llama-3.3-70b-versatile",
     temperature=0.0,
     model_kwargs={"response_format": {"type": "json_object"}}
 )
 
+# Another ChatGroq instance, this one using the 'openai/gpt-oss-120b' model.
 llm_oss = ChatGroq(
     model="openai/gpt-oss-120b",
     temperature=0.0,
     model_kwargs={"response_format": {"type": "json_object"}}
-) 
+)
 
+# ChatGroq instance for general conversational use (not JSON).
+# Uses the 'llama-3.3-70b-versatile' model for standard chat responses.
 groq_chat = ChatGroq(
     model="llama-3.3-70b-versatile",
     temperature=0.0,
 )
 
+# This one is based on the 'openai/gpt-oss-120b' model.
 chat_oss = ChatGroq(
     model="openai/gpt-oss-120b",
     temperature=0.0
-) 
+)
+
